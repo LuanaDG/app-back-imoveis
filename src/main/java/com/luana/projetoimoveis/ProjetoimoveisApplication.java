167c586a526a09e9dfaafc14753c25e804f58e78
Luana Dela Giustina <luanadelagiustina@gmail.com>
2020-06-27 09:23:07 -0300

    projeto iniciado

---
+++ src/main/java/com/luana/projetoimoveis/ProjetoimoveisApplication.java
@@ -0,0 +1,13 @@
+package com.luana.projetoimoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoimoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoimoveisApplication.class, args);
+	}
+
+}
